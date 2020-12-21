2999260f966b02fc03bdbfb9338490942bcdf8ac
Cara Zellner <carajeanine@gmail.com>
2020-12-21 18:45:18 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -13,13 +13,10 @@ class Customer
     @@all   
   end
   
-  def new_meal(customer, total, tip=0)
-    Meal.new(self, customer, total, tip)
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
   end
   
-  def meals
-    Meal.all.select do |meal|
-      meal.customer == self
-    end
-  end
+  
+  
 end
